680278d28befae97b9cbf02789fac563525565a4
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-11-10 22:22:43 +1300

    implemented abstract classes

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,21 @@ import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([1, 5, -1, 2]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([1, 5, -1, 2]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 // const charactersCollection = new CharactersCollection("axscd");
 // const sorter = new Sorter(charactersCollection);
 // sorter.sort();
 // console.log(charactersCollection.data);
 
-const linkedList = new LinkedList();
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(-3);
-linkedList.add(5);
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(5);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
-linkedList.print();
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
+// linkedList.print();
