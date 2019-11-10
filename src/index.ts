2e5731743391c2584f2c06c57b927b50c2311ade
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-11-10 14:08:24 +1300

    modularized and refactored sorting logic for numbers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hi there");
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+
+const numbersCollection = new NumbersCollection([1, 5, -1, 2]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
