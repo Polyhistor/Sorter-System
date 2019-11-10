d90e92b304cffad22db273401bc08e76d9af8fc6
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-11-10 21:21:18 +1300

    finalized linked list sorting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 // const numbersCollection = new NumbersCollection([1, 5, -1, 2]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection("axscd");
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection("axscd");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(5);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
